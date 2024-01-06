79990f1507dfd3bfb30fd860f647167ddcf7dd3a
tyfkda <tyfkda@gmail.com>
2024-01-06 11:08:40 +0900

    解を順に進めて表示する
    `--interactive`

--- README.md
+++ README.md
@@ -5,6 +5,22 @@
 
 ```sh
 $ cd rust
+$ cargo run --release -- --interactive
+step # 81: 0, ←
+3━━┓1━━┓4━━┓2━━┓
+┃<>┃┃<>┃┃<>┃┃<>┃
+┃<>┃┃<>┃┃<>┃┃<>┃
+┗━━┛┗━━┛┗━━┛┗━━┛
+5━━━━━━┓7━━┓8━━┓
+┗━━━━━━┛┗━━┛┗━━┛
+9━━┓    0━━━━━━┓
+┗━━┛   ←┃<><><>┃
+6━━┓   ←┃<><><>┃
+┗━━┛    ┗━━━━━━┛
+>
+```
+
+```sh
 $ time cargo run --release
 Solved!, steps=81, check=#19306, left=93, hash=23805
 Hands #118: [(6, Right), (3, Down), (5, Left), (8, Down), (4, Left), (9, Up), (8, Right), (4, Down), (5, Right), (5, Right), (7, Up), (7, Left), (6, Up), (6, Up), (4, Left), (9, Left), (9, Down), (5, Down), (6, Right), (6, Right), (7, Right), (7, Right), (3, Up), (4, Up), (9, Left), (9, Left), (8, Left), (8, Left), (5, Down), (7, Down), (7, Right), (4, Right), (3, Right), (1, Down), (1, Down), (0, Left), (2, Left), (6, Up), (6, Up), (7, Up), (7, Up), (4, Right), (2, Down), (2, Down), (0, Right), (1, Up), (1, Up), (3, Left), (8, Up), (8, Up), (9, Right), (9, Up), (5, Left), (5, Left), (2, Down), (4, Down), (8, Right), (8, Right), (0, Down), (6, Left), (6, Left), (7, Up), (7, Left), (8, Up), (8, Up), (4, Up), (4, Up), (2, Right), (9, Right), (9, Down), (0, Down), (6, Down), (6, Right), (1, Right), (3, Up), (3, Up), (0, Left), (6, Down), (6, Down), (7, Down), (8, Left), (4, Up), (2, Up), (9, Right), (6, Down), (0, Right), (3, Down), (3, Down), (1, Left), (7, Left), (8, Left), (4, Left), (2, Up), (2, Up), (0, Right), (7, Down), (7, Down), (8, Down), (8, Down), (1, Right), (3, Up), (3, Up), (7, Left), (7, Up), (5, Up), (6, Left), (6, Left), (9, Left), (9, Left), (0, Down), (8, Right), (8, Right), (7, Right), (7, Right), (5, Up), (9, Up), (9, Left), (0, Left)]
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,18 @@
 mod hakoiri;
 
+use clap::Parser;
 use std::error::Error;
+use std::io::{self, BufRead, Write};
 
-use hakoiri::{parse_board, solve, Pos, Piece, SPACE, BOARD_W, BOARD_H};
+use hakoiri::{parse_board, solve, Dir, Piece, Pos, SPACE, BOARD_W, BOARD_H};
+
+#[derive(Parser, Debug)]
+#[command(about, long_about = None)]
+struct Args {
+    /// Print solution interactively.
+    #[arg(long)]
+    interactive: bool,
+}
 
 fn print_board(positions: &[Pos], pieces: &[Piece]) {
     let mut s = [SPACE; BOARD_W * BOARD_H];
@@ -20,7 +30,129 @@ fn print_board(positions: &[Pos], pieces: &[Piece]) {
     }
 }
 
+fn put_arrow(s: &mut [char; (BOARD_W * 4) * (BOARD_H * 2)], x: usize, y: usize, arrow: char, dx: usize, dy: usize) {
+    let line_width = BOARD_W * 4;
+    s[y * line_width + x] = arrow;
+    s[(y + dy) * line_width + (x + dx)] = arrow;
+}
+
+fn print_interactive_board(positions: &[Pos], pieces: &[Piece], hand_opt: Option<(usize, Dir)>) {
+    let mut s = [' '; (BOARD_W * 4) * (BOARD_H * 2)];
+    let line_width = BOARD_W * 4;
+    for (pos, (c, size)) in positions.iter().zip(pieces) {
+        let w = size.w() as usize;
+        let h = size.h() as usize;
+        for i in 0..h * 2 {
+            let y = (pos.y as usize) * 2 + i;
+            for j in 0..w * 4 {
+                let x = (pos.x as usize) * 4 + j;
+                let cc = if i == 0 {
+                    if j == 0              { *c }
+                    else if j == w * 4 - 1 { '┓' }
+                    else                   { '━' }
+                } else if i == h * 2 - 1 {
+                    if j == 0              { '┗' }
+                    else if j == w * 4 - 1 { '┛' }
+                    else                   { '━' }
+                } else {
+                    if j == 0 || j == w * 4 - 1 { '┃' }
+                    else if j & 1 != 0          { '<' }
+                    else                        { '>' }
+                };
+                s[y * line_width + x] = cc;
+            }
+        }
+    }
+
+    if let Some((ip, dir)) = hand_opt {
+        let pos = &positions[ip];
+        let size = pieces[ip].1;
+        let w = size.w() as usize;
+        let h = size.h() as usize;
+        let x = (pos.x as usize) * 4;
+        let y = (pos.y as usize) * 2;
+        match dir {
+            hakoiri::Dir::Left =>  { put_arrow(&mut s, x - 1,         y + h - 1, '←', 0, 1); },
+            hakoiri::Dir::Right => { put_arrow(&mut s, x + w * 4    , y + h - 1, '→', 0, 1); },
+            hakoiri::Dir::Up =>    { put_arrow(&mut s, x + w * 2 - 1, y - 1,     '↑', 1, 0); },
+            hakoiri::Dir::Down =>  { put_arrow(&mut s, x + w * 2 - 1, y + h * 2, '↓', 1, 0); },
+        };
+    }
+
+    for i in 0..BOARD_H * 2 {
+        println!("{}", &s[(i * BOARD_W * 4)..((i + 1) * BOARD_W * 4)].iter().collect::<String>());
+    }
+}
+
+fn cls() {
+    print!("\x1b[2J");
+}
+
+fn locate(x: usize, y: usize) {
+    print!("\x1b[{};{}H", y + 1, x + 1);
+}
+
+fn clear_line_after() {
+    print!("\x1b[K");
+}
+
+fn print_solution_interactive(positions: &Vec<Pos>, pieces: &[Piece], hands: &[(usize, hakoiri::Dir)]) {
+    let mut positions = positions.clone();
+    let stdin = io::stdin();
+    let mut step = 0;
+    let mut step2 = 0;
+    let mut line = String::new();
+    let mut last = usize::MAX;
+
+    cls();
+    loop {
+        locate(0, 0);
+        clear_line_after();
+        let hand_opt = if step >= hands.len() {
+            println!("step #{step2:3}: ");
+            None
+        } else {
+            let (ip, dir) = hands[step];
+            let arrow = match dir {
+                hakoiri::Dir::Left =>  "←",
+                hakoiri::Dir::Right => "→",
+                hakoiri::Dir::Up =>    "↑",
+                hakoiri::Dir::Down =>  "↓",
+            };
+            println!("step #{:3}: {:}, {arrow:}", step2 + 1, pieces[ip].0);
+            Some((ip, dir))
+        };
+        print_interactive_board(&positions, pieces, hand_opt);
+        clear_line_after();
+        if step >= hands.len() {
+            println!("Solved!");
+            break;
+        }
+
+        // Wait input.
+        print!("> ");
+        io::stdout().flush().unwrap();
+        stdin.lock().read_line(&mut line).unwrap();
+
+        let (ip, dir) = hands[step];
+        let pos = &mut positions[ip];
+        match dir {
+            hakoiri::Dir::Left =>  pos.x -= 1,
+            hakoiri::Dir::Right => pos.x += 1,
+            hakoiri::Dir::Up =>    pos.y -= 1,
+            hakoiri::Dir::Down =>  pos.y += 1,
+        }
+        step += 1;
+        if last != ip {
+            step2 += 1;
+        }
+        last = ip;
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
     let initial_arrange: String = [
         "1002",
         "1002",
@@ -35,9 +167,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Ok(());
     };
 
-    println!("Solved!, steps={steps}");
-    println!("Hands #{}: {:?}", hands.len(), &hands);
-    print_board(&solved_positions, &pieces);
+    if args.interactive {
+        print_solution_interactive(&positions, &pieces, &hands);
+    } else {
+        println!("Solved!, steps={steps}");
+        println!("Hands #{}: {:?}", hands.len(), &hands);
+        print_board(&solved_positions, &pieces);
+    }
 
     Ok(())
 }
